402181803f8bed456cbde9a0296ecdf335b36372
Dawid Majchrzak <programista.dawid.majchrzak@gmail.com>
2021-08-28 19:26:04 +0200

    Building Header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Header.css';
-import TopContactInfo from '../TopContactInfo/TopContactInfo';
-// import Logo from './constmach-polska-logo';
+import TopContactInfo from './TopContactInfo/TopContactInfo';
+import Logo from './Logo.png'
 
 
 export default function Header() {
     return (
         <div>
             <TopContactInfo />
-            {/* <Logo /> */}
-            <div className="logo">LOGO</div>
+            <div className="logo-container">
+                <img src={Logo} alt="logo" />
+                <div className="slider">SLIDER</div>
+            </div>
         </div>
     )
 }
